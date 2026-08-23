7b6ba0e4cbd31928a3bd0aecd6fadcba0be5fbcb
Tinar04 <rathoretina76@gmail.com>
2026-08-23 23:57:15 +0530

    Added a details view and template to show the details of a perticular course later ill add css

--- Course portal/course_portal/courses/urls.py
+++ Course portal/course_portal/courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.display_course_view),
+    path('details/<str:code_id>',views.details_of_course_view,name='details'),
 ]
--- Course portal/course_portal/courses/views.py
+++ Course portal/course_portal/courses/views.py
@@ -7,3 +7,15 @@ def display_course_view(request):
         'courses':COURSES
     }
     return render(request,'display_courses.html',context)
+
+def details_of_course_view(request,code_id):
+    for code,course in COURSES.items():
+        if code.lower() == code_id.lower() :
+            data = course
+            break
+    else:
+        data = None
+    context = {
+        'course':data
+    }
+    return render(request,'details_of_course.html', context)
