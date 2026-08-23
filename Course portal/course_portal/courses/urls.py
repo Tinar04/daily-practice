77ffdeffe09004888c69d131d8911515f19105b1
Tinar04 <rathoretina76@gmail.com>
2026-08-24 01:17:06 +0530

    Added CSS and Navigation to the home page

--- Course portal/course_portal/courses/urls.py
+++ Course portal/course_portal/courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.display_course_view),
+    path('',views.display_course_view,name = 'courses'),
     path('details/<str:code_id>',views.details_of_course_view,name='details'),
 ]
