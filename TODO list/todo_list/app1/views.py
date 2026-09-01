df00b88ed79c5df0314e02f31c58429a269dc56a
Tinar04 <rathoretina76@gmail.com>
2026-09-01 22:57:49 +0530

    Added card

--- TODO list/todo_list/app1/views.py
+++ TODO list/todo_list/app1/views.py
@@ -12,6 +12,7 @@ def task_form_view(request):
         print("data inside form object")
         if form.is_valid():
             print("validation done",form.cleaned_data)
+
             Task_Model.objects.create(
                 task_name = form.cleaned_data['task_name'],
                 task_description = form.cleaned_data['task_description'])
@@ -43,10 +44,3 @@ def task_form_view(request):
 
         
 
-       
-
-    # form = Task_Form()
-    # context = {
-    #     "form":form
-    # }
-    # return render(request,'Task_page.html',context)
