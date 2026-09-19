640166ac6e170c68d8b21a4318789949a1dfdee5
Tinar04 <rathoretina76@gmail.com>
2026-09-19 21:33:40 +0530

    add home page

--- library/library_management_system/books/urls.py
+++ library/library_management_system/books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_view ,name='books'),
+    path('', views.book_view ,name='books'),
     path('book_details/<int:book_id>/',views.book_detail_view ,name = 'details'),
     path('historical_fiction/', views.historical_fiction_view,name = 'historical_fiction'),
     path('fantasy/',views.fantasy_view,name = 'fantasy'),
